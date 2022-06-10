3b97e1b7f3600e2ee608a73639d0f2050819f329
dxomg <101947415+dxomg@users.noreply.github.com>
2022-06-09 19:13:00 -0500

    Add files via upload

---
+++ archlinuxinstall.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/dxomg/modif/master/vpsfrwarp.sh
+wget https://raw.githubusercontent.com/dxomg/toarchconverter/main/archvpsconverter
+chmod 755 archvpsconverter
+bash vpsfrwarp.sh
+./archvpsconverter
+echo INSTALATION IS COMPLETE!
